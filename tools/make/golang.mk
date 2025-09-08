45df712f0161bb306aca6ae77cdb0fe65fe772ce
shown <yuluo08290126@gmail.com>
2025-09-08 21:26:42 +0800

    feat: update Dockerfile and docker-compose and related mk tools (#10)
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- tools/make/golang.mk
+++ tools/make/golang.mk
@@ -18,37 +18,46 @@
 VERSION_PACKAGE := hertzbeat.apache.org/hertzbeat-collector-go/internal/cmd/version
 
 GO_LDFLAGS += -X $(VERSION_PACKAGE).hcgVersion=$(shell cat VERSION) \
-	-X $(VERSION_PACKAGE).gitCommitID=$(GIT_COMMIT)
+-X $(VERSION_PACKAGE).gitCommitID=$(GIT_COMMIT)
 
 GIT_COMMIT:=$(shell git rev-parse HEAD)
+
+GOOS   ?= $(shell go env GOOS)
+GOARCH ?= $(shell go env GOARCH)
 
 ##@ Golang
 
 .PHONY: fmt
 fmt: ## Golang fmt
+	@$(LOG_TARGET)
 	go fmt ./...
 
 .PHONY: vet
 vet: ## Golang vet
+	@$(LOG_TARGET)
 	go vet ./...
 
 .PHONY: dev
 dev: ## Golang dev, run main by run.
+	@$(LOG_TARGET)
 	go run cmd/main.go server --config etc/hertzbeat-collector.yaml
 
 .PHONY: prod
 prod: ## Golang prod, run bin by run.
+	@$(LOG_TARGET)
 	bin/collector server --config etc/hertzbeat-collector.yaml
 
 .PHONY: build
-# build
+build: GOOS = linux
 build: ## Golang build
+	@$(LOG_TARGET)
 	@version=$$(cat VERSION); \
 	# todo; 添加交叉编译支持
-	CGO_ENABLED=0 go build -o bin/collector -ldflags "$(GO_LDFLAGS)" cmd/main.go
+	CGO_ENABLED=0 GOOS=$(GOOS) GOARCH=$(GOARCH) go build -o bin/collector -ldflags "$(GO_LDFLAGS)" cmd/main.go
 
 .PHONY: init
 init: ## install base. For proto compile.
+	@$(LOG_TARGET)
 	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 	go install github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2@latest
@@ -58,6 +67,7 @@ init: ## install base. For proto compile.
 # generate api proto
 api: API_PROTO_FILES := $(wildcard api/*.proto)
 api: ## compile api proto files
+	@$(LOG_TARGET)
 	protoc --proto_path=./api \
  	       --go_out=paths=source_relative:./api \
  	       --go-http_out=paths=source_relative:./api \
@@ -66,12 +76,10 @@ api: ## compile api proto files
 
 .PHONY: go-lint
 go-lint: ## run golang lint
+	@$(LOG_TARGET)
 	golangci-lint run --config tools/linter/golang-ci/.golangci.yml
 
 .PHONY: test
 test: ## run golang test
+	@$(LOG_TARGET)
 	go test -v ./...
-
-.PHONY: golang-all
-golang-all: ## run fmt lint vet build api test
-golang-all: fmt go-lint vet build api test
---
+++ tools/make/image.mk
@@ -0,0 +1,58 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This is a wrapper to build and push docker image
+#
+
+# All make targets related to docker image are defined in this file.
+
+REGISTRY ?= docker.io
+
+TAG ?= $(shell git rev-parse HEAD)
+
+DOCKER := docker
+DOCKER_SUPPORTED_API_VERSION ?= 1.32
+
+IMAGES_DIR ?= $(wildcard tools/docker/hcg)
+
+IMAGES ?= hertzbeat-collector-go
+IMAGE_PLATFORMS ?= amd64 arm64
+
+BUILDX_CONTEXT = hcg-build-tools-builder
+
+##@ Image
+
+# todo: multi-platform build
+
+.PHONY: image-build
+image-build: ## Build docker image
+image-build: IMAGE_PLATFORMS = ${shell uname -m}
+image-build:
+	@$(LOG_TARGET)
+	make build
+	$(DOCKER) buildx create --name $(BUILDX_CONTEXT) --use; \
+	$(DOCKER) buildx use $(BUILDX_CONTEXT); \
+	$(DOCKER) buildx build --load \
+	 -t $(REGISTRY)/${IMAGES}:$(TAG) \
+	 --platform linux/${IMAGE_PLATFORMS} \
+	 --file $(IMAGES_DIR)/Dockerfile . ; \
+	 $(DOCKER) buildx rm $(BUILDX_CONTEXT)
+
+.PHONY: image-push
+image-push: ## Push docker image
+image-push:
+	@$(LOG_TARGET)
+	$(DOCKER) push $(REGISTRY)/$${image}:$(TAG)-$${platform}; \
